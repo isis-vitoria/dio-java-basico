c30a254fcc098bea11ac74afbb17c7ee86cc3514
isis vitoria vilar cunha <isis.vilar@gmail.com>
2024-07-26 16:38:19 -0300

    subindo a atividade

--- README.md
+++
@@ -1,8 +0,0 @@
-# dio-java-basico-
-MODULO 2 
-public Class MinhaClasse {
- public static void main (String [] args) {
-    system.out.print ("ola turma"); 
-    }
-}
-
---
+++ exercicio-validaçao-processo-seletivo/src/Contador.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parametro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parametro");
+        int parametroDois = terminal.nextInt();
+        
+        try {
+           contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosException exception) {
+           System.out.println(exception.getMessage());
+        }
+    }
+    
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException("O segundo parametro deve ser maior que o primeiro");
+        }
+        
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o numero " + i);
+        }
+    }
+}
